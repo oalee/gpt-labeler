c0519e8994e8015a1037c8e3e0074f1333555475
oalee <oalee@proton.me>
2023-05-29 10:21:21 +0200

    adding src

---
+++ src/DataVisualization.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import data from './pers_labels_history.json';
+import './DataVisualization.css'; // Import the CSS file
+
+const DataVisualization = () => {
+  // Parse the JSON data
+
+    const [jsonData, setJsonData] = useState(data);
+    // State to track the collapsed state of user data
+
+    const [collapsedItems, setCollapsedItems] = useState({});
+
+    // use effect to set to false initially
+    useEffect(() => {
+      const initialCollapsedItems = {};
+      Object.keys(jsonData).forEach((tweetId) => {
+        initialCollapsedItems[tweetId] = true;
+      });
+      setCollapsedItems(initialCollapsedItems);
+    }, [jsonData]);
+
+
+    const handleToggle = (tweetId) => {
+        console.log('tweetId', tweetId);
+      setCollapsedItems((prevState) => ({
+        ...prevState,
+        [tweetId]: !prevState[tweetId],
+        }));
+    };
+  
+    return (
+      <div className="container">
+        <div>
+          <h1>Tweet Analysis</h1>
+          {Object.keys(jsonData).map((tweetId) => {
+            const tweet = jsonData[tweetId];
+            const history = tweet.history;
+            const tweetText = tweet.item.rawContent;
+            const isCollapsed = collapsedItems[tweetId];
+            const isValidated = tweet.isValidated || false;
+
+            return (
+              <div className="card" key={tweetId}>
+                <div className="card-header" onClick={() => handleToggle(tweetId)}>
+                  <h2>Tweet ID: {tweetId}</h2>
+                  <h3>{tweetText}</h3>
+                </div>
+                <div className={`card-body ${isCollapsed ? 'collapsed' : ''}`} style={{ display: isCollapsed ? 'none' : 'block' }}>                  {history.map((item, index) => (
+                    <div className="item" key={index}>
+                      <h3>Role: {item.role}</h3>
+                      {item.role === 'user' && <p>User Data: {item.data}</p>}
+                      {item.role === 'assistant' && (
+                        <div>
+                          <p>Parsed Output:</p>
+                          <pre>{JSON.stringify(item.parsedOutput, null, 2)}</pre>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+  
+  export default DataVisualization;
